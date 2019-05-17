ddb923eb97b0d80fc2a359f4d956d1910d084a4d
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-05-17 07:03:36 -0700

    Added tests for prop passing.

--- src/__tests__/widgets/testGenericWidget.test.jsx
+++ src/__tests__/widgets/testGenericWidget.test.jsx
@@ -7,3 +7,22 @@ it('renders without crashing', () => {
   /* eslint-disable-next-line */
   const wrapper = shallow(<GenericInput name="myInput" />);
 });
+
+it('passes name to id and name fields', () => {
+  const wrapper = shallow(<GenericInput name="myInput" />);
+
+  expect(wrapper.prop('id')).toBe('myInput');
+  expect(wrapper.prop('name')).toBe('myInput');
+});
+
+it('passes attributes in attrs to raw html', () => {
+  let attrs = { pattern: '[0-9]{3}' };
+  let wrapper = shallow(<GenericInput name="myInput" attrs={attrs} />);
+
+  expect(wrapper.prop('pattern')).toBe('[0-9]{3}');
+
+  attrs = { 'data-target': 'some_val' };
+  wrapper = shallow(<GenericInput name="myInput" attrs={attrs} />);
+
+  expect(wrapper.prop('data-target')).toBe('some_val');
+});
