eace59c1a6629c632136fd546216eb9c265c492b
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2019-05-17 06:36:17 -0700

    Setup eslint how I prefer it.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: 'airbnb',
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
